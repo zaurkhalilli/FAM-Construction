33afa1460329b1a97980a3d1c24f67827c783644
Zaur Khalilli <khalillizaur1@gmail.com>
2025-04-10 13:20:19 +0400

    bootstrap elave olundu

---
+++ fam-construction/src/router.jsx
@@ -0,0 +1,29 @@
+const router=[
+    {   id:1,
+        name:"home",
+        path:"/"
+    },
+    {
+        id:2,
+        name:"haqqimizda",
+        path: "/haqqimizda"
+    },
+    {
+        id:3,
+        name:"xidmetler",
+        path:"/xidmetler"
+    },
+    {
+        id:4,
+        name:"lahiyeler",
+        path:"/lahiyeler"
+    },
+    {
+        id:5,
+        name:"elaqe",
+        path:"/elaqe"
+    },
+]
+
+
+export default router
